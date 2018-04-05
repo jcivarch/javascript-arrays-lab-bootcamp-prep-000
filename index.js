76cebd0d7aa69ed333b46c60d0bbb4d387fb0e3d
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-05 03:17:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,32 +3,3 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   return kitten.push(name)
 }
-
-function destructivelyPrependKitten(name) {
-  
-}
-
-function destructivelyRemoveLastKitten(name) {
-  
-}
-
-function destructivelyRemoveFirstKitten(name) {
-  
-}
-
-function appendKitten(name) {
-  
-}
-
-function prependKitten(name) {
-  
-}
-
-function removeLastKitten() {
-  
-}
-
-function removeFirstKitten() {
-  
-}
-
