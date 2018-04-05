648e54f4f20f7e274c842b870a3a55168bf9e651
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-05 03:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const kitten = ["Milo", "Otis", "Garfield"]
+const kitten = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
-  return kitten.push(name)
+  return kitten.push(name);
 }
+destructivelyAppendKitten("Ralph");
+
